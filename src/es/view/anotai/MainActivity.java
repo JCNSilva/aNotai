50fc537df5043f8c0b46ac038535ab60e636d141
aparecidoSilvano <silvanoalbuquerque@gmail.com>
2015-10-11 23:19:07 -0300

    Exibe na tela principal detalhes da próxima atividades.

--- src/es/view/anotai/MainActivity.java
+++ src/es/view/anotai/MainActivity.java
@@ -1,11 +1,16 @@
 package es.view.anotai;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
+import es.database.anotai.TaskPersister;
+import es.model.anotai.Task;
 import projeto.es.view.anotai.R;
 
 public class MainActivity extends Activity {
@@ -21,6 +26,44 @@ public class MainActivity extends Activity {
 		addClickListenerPerformance();
 		addClickListenerHomeWork();
 
+		updateNextTask();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		updateNextTask();
+	}
+
+	private void updateNextTask() {
+		TaskPersister persister = new TaskPersister(this);
+		Task task = getNextTask(persister);
+		TextView tvInfoTask = (TextView) findViewById(R.id.tv_info_tasks);
+		if (task != null) {
+			tvInfoTask.setText(getResources().getString(R.string.next_activity));
+
+			TextView tvNameTask = (TextView) findViewById(R.id.tv_name_task);
+			tvNameTask.setText(task.getDescription());
+
+			TextView tvNameDiscipline = (TextView) findViewById(R.id.tv_name_discipline_task);
+			tvNameDiscipline.setText(getResources().getString(R.string.discipline) + " " + task.getDiscipline().getName());
+		} else {
+			tvInfoTask.setText(getResources().getString(R.string.no_next_activity));
+		}
+	}
+
+	private Task getNextTask(TaskPersister persister) {
+		List<Task> tasks = persister.retrieveAll();
+		if (!tasks.isEmpty()) {
+			Task next = tasks.get(0);
+			for (Task t : persister.retrieveAll()) {
+				if (t.getDeadlineDate().before(next.getDeadlineDate())) {
+					next = t;
+				}
+			}
+			return next;
+		}
+		return null;
 	}
 
 	private void addClickListenerHomeWork() {
