28bb0859cb15cb94fe14f3b9f5a8ef9135fa1a55
Julio Cesar Neves <julio.cesar.silva@ccc.ufcg.edu.br>
2015-10-14 22:42:50 -0300

    Remoção de disciplinas ligada ao bd

--- src/es/view/anotai/DisciplinesActivity.java
+++ src/es/view/anotai/DisciplinesActivity.java
@@ -15,11 +15,13 @@ import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.util.Log;
 import android.view.ActionMode;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
@@ -36,67 +38,71 @@ import es.database.anotai.TaskPersister;
 import es.model.anotai.Discipline;
 
 public class DisciplinesActivity extends Activity {
+	public static final String CLASS_TAG = "DisciplinesActivity";
+	
 	private DisciplinePersister dPersister;
 	private TaskPersister tPersister;
-    private List<Discipline> disciplines;
-    private ListView lvDisciplines;
-    private SortOrderDiscipline currentSortEstrat; //TODO Strategy
-    private ActionMode mActionMode;
-    private ActionMode.Callback mActionModeCallback;
-    
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        setContentView(R.layout.activity_disciplines);
-        
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        
-        initializeFields();
-        
-        addClickListenerAddDiscipline();
-        initializeActionModeCallback();
-        addClickListenerSortButton();
-        addClickListenerLvDisciplines();
-        addContextMenuLvDisciplines();
-        
-        loadList();         
-    }
+	private List<Discipline> disciplines;
+	private ListView lvDisciplines;
+	private SortOrderDiscipline currentSortEstrat; // TODO Strategy
+	private ActionMode mActionMode;
+	private ActionMode.Callback mActionModeCallback;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_disciplines);
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+
+		initializeFields();
+
+		addClickListenerAddDiscipline();
+		initializeActionModeCallback();
+		addClickListenerSortButton();
+		addClickListenerLvDisciplines();
+		addContextMenuLvDisciplines();
+
+		loadList();
+	}
 
 	private void initializeFields() {
 		tPersister = new TaskPersister(this);
-        dPersister = new DisciplinePersister(this);
-        disciplines = dPersister.retrieveAll();
-        currentSortEstrat = SortOrderDiscipline.TOTAL_TASKS_DESC;
-        lvDisciplines = (ListView) findViewById(R.id.activity_disciplines_lv_disciplines);
+		dPersister = new DisciplinePersister(this);
+		disciplines = dPersister.retrieveAll();
+		currentSortEstrat = SortOrderDiscipline.TOTAL_TASKS_DESC;
+		lvDisciplines = (ListView) findViewById(R.id.activity_disciplines_lv_disciplines);
+		lvDisciplines.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);		
 	}
 
 	private void addContextMenuLvDisciplines() {
 		lvDisciplines.setOnItemLongClickListener(new OnItemLongClickListener() {
-  			
-  			@Override
-			public boolean onItemLongClick(AdapterView<?> parent,
-					View view, int position, long id) {
-				if (mActionMode != null) {
-  		            return false;
-  		        }
 
-  		        // Start the CAB using the ActionMode.Callback defined above
-  		        mActionMode = startActionMode(mActionModeCallback);
-  		        lvDisciplines.setSelected(true);
-  		        return true;
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				if (mActionMode != null) {
+					return false;
+				}
+
+				// Start the CAB using the ActionMode.Callback defined above
+				lvDisciplines.setItemChecked(position, true);
+				mActionMode = startActionMode(mActionModeCallback);				
+				return true;
 			}
-  		});
+		});
 	}
 
 	private void addClickListenerLvDisciplines() {
 		lvDisciplines.setOnItemClickListener(new OnItemClickListener() {
-    		@Override
+			@Override
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
-				// Passar a disciplina clicada na intent que vai a tela da disciplina
-				Intent intent = new Intent(DisciplinesActivity.this, DisciplineActivity.class);
+				// Passar a disciplina clicada na intent que vai a tela da
+				// disciplina
+				Intent intent = new Intent(DisciplinesActivity.this,
+						DisciplineActivity.class);
 				intent.putExtra("discipline", disciplines.get(position));
 				startActivity(intent);
 			}
@@ -105,17 +111,17 @@ public class DisciplinesActivity extends Activity {
 
 	private void addClickListenerSortButton() {
 		ImageButton sortButton = (ImageButton) findViewById(R.id.activity_disciplines_ibt_sort);
-        sortButton.setOnClickListener(new OnClickListener() {
-			
+		sortButton.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				PopupMenu popup = new PopupMenu(DisciplinesActivity.this, v);
-				
+
 				popup.setOnMenuItemClickListener(new OnMenuItemClickListener() {
-					
+
 					@Override
 					public boolean onMenuItemClick(MenuItem item) {
-						switch(item.getItemId()){
+						switch (item.getItemId()) {
 						case R.id.ordering_disciplines_alphabetical:
 							changeSortEstrat(SortOrderDiscipline.ALPHABETICAL);
 							loadList();
@@ -129,47 +135,49 @@ public class DisciplinesActivity extends Activity {
 						}
 					}
 				});
-				
+
 				popup.inflate(R.menu.ordering_disciplines);
 				popup.show();
 			}
 		});
 	}
-    
-    // Criando menu de contexto para listview
-    private void initializeActionModeCallback() {
+
+	// Criando menu de contexto para listview
+	private void initializeActionModeCallback() {
 		mActionModeCallback = new ActionMode.Callback() {
-			
+
 			@Override
 			public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
 				return false;
 			}
-			
+
 			@Override
 			public void onDestroyActionMode(ActionMode mode) {
-				mActionMode = null;				
+				mActionMode = null;
 			}
-			
+
 			@Override
 			public boolean onCreateActionMode(ActionMode mode, Menu menu) {
 				MenuInflater inflater = mode.getMenuInflater();
 				inflater.inflate(R.menu.options, menu);
 				return true;
 			}
-			
+
 			@Override
 			public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-				switch(item.getItemId()){
+				switch (item.getItemId()) {
 				case R.id.menu_options_edit:
+					EditDisciplineDialogFragment myEditDiscDF = new EditDisciplineDialogFragment();
+					myEditDiscDF.show(getFragmentManager(), myEditDiscDF.getTag());
 					mode.finish();
-					//TODO edit activity
 					loadList();
 					return true;
 				case R.id.menu_options_delete:
-					ConfirmDeleteDialogFragment myConfirmDelDF = 
-						new ConfirmDeleteDialogFragment((Discipline) lvDisciplines.getSelectedItem());
-					myConfirmDelDF.show(getFragmentManager(), myConfirmDelDF.getTag());
+					ConfirmDeleteDialogFragment myConfirmDelDF = new ConfirmDeleteDialogFragment(); 
+					myConfirmDelDF.show(getFragmentManager(),
+							myConfirmDelDF.getTag());
 					mode.finish();
+					loadList();
 					return true;
 				default:
 					return false;
@@ -180,180 +188,222 @@ public class DisciplinesActivity extends Activity {
 
 	private void addClickListenerAddDiscipline() {
 		Button btAddDiscipline = (Button) findViewById(R.id.activity_disciplines_bt_add_discipline);
-        btAddDiscipline.setOnClickListener(new OnClickListener() {
+		btAddDiscipline.setOnClickListener(new OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                final Dialog dialog = new Dialog(DisciplinesActivity.this);
-                dialog.setContentView(R.layout.dialog_add_discipline);
-                dialog.setTitle(R.string.new_discipline);
+			@Override
+			public void onClick(View v) {
+				final Dialog dialog = new Dialog(DisciplinesActivity.this);
+				dialog.setContentView(R.layout.dialog_add_discipline);
+				dialog.setTitle(R.string.new_discipline);
 
-                // recuperando os elementos do dialog
-                final EditText editNameContact = (EditText) dialog
-                        .findViewById(R.id.dialog_add_disc_et_name_discipline);
-                
-                final Button btOk = (Button) dialog.findViewById(R.id.dialog_add_disc_bt_ok);
-                final Button btCancel = (Button) dialog.findViewById(R.id.dialog_add_disc_bt_cancel);
+				// recuperando os elementos do dialog
+				final EditText editNameContact = (EditText) dialog
+						.findViewById(R.id.dialog_add_disc_et_name_discipline);
 
-                btOk.setOnClickListener(new OnClickListener() {
+				final Button btOk = (Button) dialog
+						.findViewById(R.id.dialog_add_disc_bt_ok);
+				final Button btCancel = (Button) dialog
+						.findViewById(R.id.dialog_add_disc_bt_cancel);
 
-                    @Override
-                    public void onClick(final View v) {
-                        final String name = editNameContact.getText().toString();
+				btOk.setOnClickListener(new OnClickListener() {
 
-                        if (name.isEmpty()) {
-                         // Mostra mensagem de erro.
-                            Toast.makeText(getApplicationContext(),
-                                    getResources().getString(R.string.invalid_name),
-                                    Toast.LENGTH_LONG).show();
-                        } else {
-                            Discipline discipline = new Discipline(name, "");
-                            dPersister.create(discipline);
-                            disciplines.add(discipline);
-                            Log.d(STORAGE_SERVICE, "Salva disciplina: " + name);
-                            
-                            // Atualiza a lista.
-                            loadList();
-                            dialog.dismiss();
-                        }
-                    }
-                });
+					@Override
+					public void onClick(final View v) {
+						final String name = editNameContact.getText()
+								.toString();
 
-                btCancel.setOnClickListener(new OnClickListener() {
+						if (name.isEmpty()) {
+							// Mostra mensagem de erro.
+							Toast.makeText(
+									getApplicationContext(),
+									getResources().getString(
+											R.string.invalid_name),
+									Toast.LENGTH_LONG).show();
+						} else {
+							Discipline discipline = new Discipline(name, "");
+							dPersister.create(discipline);
+							disciplines.add(discipline);
+							Log.d(CLASS_TAG, "Salva disciplina: " + name);
 
-                    @Override
-                    public void onClick(final View v) {
-                        dialog.dismiss();
-                    }
-                });
+							// Atualiza a lista.
+							loadList();
+							dialog.dismiss();
+						}
+					}
+				});
 
-                dialog.show();
-            }
-        });
+				btCancel.setOnClickListener(new OnClickListener() {
+
+					@Override
+					public void onClick(final View v) {
+						dialog.dismiss();
+					}
+				});
+
+				dialog.show();
+			}
+		});
 	}
-    
-    private void changeSortEstrat(SortOrderDiscipline newSortEstrat){
-    	if(newSortEstrat == SortOrderDiscipline.ALPHABETICAL) {
-    		
-    		if(currentSortEstrat == SortOrderDiscipline.ALPHABETICAL_ASC){
-    			currentSortEstrat = SortOrderDiscipline.ALPHABETICAL_DESC;
-	    	} else {
-	    		currentSortEstrat = SortOrderDiscipline.ALPHABETICAL_ASC;	    		
-	    	}
-    		
-	    } else {
-	    	
-	    	if(currentSortEstrat == SortOrderDiscipline.TOTAL_TASKS_DESC){
-	    		currentSortEstrat = SortOrderDiscipline.TOTAL_TASKS_ASC;
+
+	private void changeSortEstrat(SortOrderDiscipline newSortEstrat) {
+		if (newSortEstrat == SortOrderDiscipline.ALPHABETICAL) {
+
+			if (currentSortEstrat == SortOrderDiscipline.ALPHABETICAL_ASC) {
+				currentSortEstrat = SortOrderDiscipline.ALPHABETICAL_DESC;
+			} else {
+				currentSortEstrat = SortOrderDiscipline.ALPHABETICAL_ASC;
+			}
+
+		} else {
+
+			if (currentSortEstrat == SortOrderDiscipline.TOTAL_TASKS_DESC) {
+				currentSortEstrat = SortOrderDiscipline.TOTAL_TASKS_ASC;
 			} else {
 				currentSortEstrat = SortOrderDiscipline.TOTAL_TASKS_DESC;
 			}
-    	}
-    }
-    
-    private void loadList() {
-    	switch(currentSortEstrat){
-    	case ALPHABETICAL_ASC:
-    		Collections.sort(disciplines, new AlphabeticalComparator());
-    		Log.d("DisciplinesActivity", "Ordenação alfabética crescente");
-    		break;
-    	case ALPHABETICAL_DESC:
-    		Collections.sort(disciplines, new AlphabeticalComparator());
-			Collections.reverse(disciplines);
-			Log.d("DisciplinesActivity", "Ordenação alfabética decrescente");
-    		break;
-    	case TOTAL_TASKS_ASC:
-    		Collections.sort(disciplines, new TotalTasksComparator());
-    		Log.d("DisciplinesActivity", "Ordenação por tarefas crescente");
+		}
+	}
+
+	private void loadList() {
+		switch (currentSortEstrat) {
+		case ALPHABETICAL_ASC:
+			Collections.sort(disciplines, new AlphabeticalComparator());
+			Log.d(CLASS_TAG, "Ordenação alfabética crescente");
 			break;
-    	case TOTAL_TASKS_DESC:
-    		Collections.sort(disciplines, new TotalTasksComparator());
+		case ALPHABETICAL_DESC:
+			Collections.sort(disciplines, new AlphabeticalComparator());
 			Collections.reverse(disciplines);
-			Log.d("DisciplinesActivity", "Ordenação por tarefas decrescente");
+			Log.d(CLASS_TAG, "Ordenação alfabética decrescente");
+			break;
+		case TOTAL_TASKS_ASC:
+			Collections.sort(disciplines, new TotalTasksComparator());
+			Log.d(CLASS_TAG, "Ordenação por tarefas crescente");
+			break;
+		case TOTAL_TASKS_DESC:
+			Collections.sort(disciplines, new TotalTasksComparator());
+			Collections.reverse(disciplines);
+			Log.d(CLASS_TAG, "Ordenação por tarefas decrescente");
 			break;
 		default:
 			break;
-    	}
-    	
-    	DisciplineAdapter adapter = new DisciplineAdapter(DisciplinesActivity.this, disciplines);
-        lvDisciplines.setAdapter(adapter);
-    }
-	
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	switch (item.getItemId()) {
+		}
+
+		DisciplineAdapter adapter = new DisciplineAdapter(
+				DisciplinesActivity.this, disciplines);
+		lvDisciplines.setAdapter(adapter);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
 		case android.R.id.home:
 			NavUtils.navigateUpFromSameTask(this);
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
 		}
-    }  
-    
-    
-    private enum SortOrderDiscipline {
-    	ALPHABETICAL, ALPHABETICAL_ASC, ALPHABETICAL_DESC, 
-    	TOTAL_TASKS, TOTAL_TASKS_ASC, TOTAL_TASKS_DESC
-    }
-    
-    
-    private class ConfirmDeleteDialogFragment extends DialogFragment {
-    	
-    	private Discipline discSelected;
-    	
-    	public ConfirmDeleteDialogFragment(Discipline discSelected) {
-			this.discSelected = discSelected;
+	}
+
+	private enum SortOrderDiscipline {
+		ALPHABETICAL, ALPHABETICAL_ASC, ALPHABETICAL_DESC, TOTAL_TASKS, TOTAL_TASKS_ASC, TOTAL_TASKS_DESC
+	}
+
+	private class EditDisciplineDialogFragment extends DialogFragment {
+		
+		private int checkedPosition = lvDisciplines.getCheckedItemPosition();
+		private Discipline discSelected = (Discipline) lvDisciplines.getItemAtPosition(checkedPosition);
+		
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+			LayoutInflater inflater = getActivity().getLayoutInflater();
+			
+			builder.setView(inflater.inflate(R.layout.dialog_edit_discipline, null));
+			builder.setTitle(R.string.edit_discipline);
+
+			AlertDialog newAlert = builder.create();
+			final EditText etNameDiscipline = (EditText) newAlert.findViewById(R.id.dialog_edit_disc_et_name);
+			final EditText etNameTeacher = (EditText) newAlert.findViewById(R.id.dialog_edit_disc_et_teacher);
+			
+			
+			builder.setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+						discSelected.setName(etNameDiscipline.getText().toString());	
+						discSelected.setTeacher(etNameTeacher.getText().toString());
+						dPersister.update(discSelected);
+						Log.i(CLASS_TAG, "Disciplina atualizada: " + discSelected.getName());
+				}
+			});
+			
+			builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					// Não fazer nada
+					
+				}
+			});
+			
+			return builder.create();
+			
 		}
-    	
-    	@Override
-    	public Dialog onCreateDialog(Bundle savedInstanceState) {
-    		// Use the Builder class for convenient dialog construction
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            
-            builder.setMessage(R.string.confirm_delete_discipline);
-            
-            builder.setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
-                	   @Override
-                       public void onClick(DialogInterface dialog, int id) {
-                    	   	dPersister.delete(discSelected);
-                    	   	loadList();
-       					}
-                   });
-            
-            builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-            			@Override
-            			public void onClick(DialogInterface dialog, int id) {
-                           // Dialogo cancelado, não faça nada
-            			}
-                   });
-            
-            return builder.create();
-    	}
-    }       
-    
-    
-    private class AlphabeticalComparator implements Comparator<Discipline> {
-    	
-    	@Override
+	}
+	
+	private class ConfirmDeleteDialogFragment extends DialogFragment {
+
+		private int checkedPosition = lvDisciplines.getCheckedItemPosition();
+		private Discipline discSelected = (Discipline) lvDisciplines.getItemAtPosition(checkedPosition);
+		
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+			builder.setMessage(R.string.confirm_delete_discipline);
+
+			builder.setPositiveButton(R.string.confirm,
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int id) {
+							dPersister.delete(discSelected);
+							Log.i(CLASS_TAG, "Disciplina Removida: " + discSelected.getName());						
+						}
+					});
+
+			builder.setNegativeButton(R.string.cancel,
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int id) {
+							// Dialogo cancelado, não faça nada
+						}
+					});
+
+			return builder.create();
+		}
+	}
+
+	private class AlphabeticalComparator implements Comparator<Discipline> {
+
+		@Override
 		public int compare(Discipline left, Discipline right) {
 			return left.getName().compareTo(right.getName());
 		}
-    }
-      
-    
-    private class TotalTasksComparator implements Comparator<Discipline> {
-    	
-    	@Override
+	}
+
+	private class TotalTasksComparator implements Comparator<Discipline> {
+
+		@Override
 		public int compare(Discipline left, Discipline right) {
 			int leftTasks = tPersister.retrieveAll(left).size();
 			int rightTasks = tPersister.retrieveAll(right).size();
-			if(leftTasks > rightTasks){
+			if (leftTasks > rightTasks) {
 				return 1;
-			} else if(leftTasks < rightTasks){
+			} else if (leftTasks < rightTasks) {
 				return -1;
 			} else {
 				return new AlphabeticalComparator().compare(left, right);
 			}
 		}
-    }
+	}
 }
