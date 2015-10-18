9d0edea31b522254436b0e135861df8b0ee6ad43
aparecidoSilvano <silvanoalbuquerque@gmail.com>
2015-10-18 20:53:00 -0300

    Exibe gráfico de desempenho nos detalhes das disciplinas.

--- src/es/view/anotai/DisciplineActivity.java
+++ src/es/view/anotai/DisciplineActivity.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.LinearLayout;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import es.adapter.anotai.TaskAdapter;
@@ -22,24 +23,24 @@ import es.model.anotai.Task;
 public class DisciplineActivity extends Activity {
 	private Discipline discipline;
 	private TaskPersister tPersister;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_discipline);
-		
+
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 
 		Bundle extras = getIntent().getExtras();
 		discipline = (Discipline) extras.get("discipline");
 		tPersister = new TaskPersister(this);
-		
+
 		ListView listView = (ListView) findViewById(R.id.activity_discipline_lv_tasks);
-		
+
 		final List<Task> tasks = tPersister.retrieveAll(discipline);
 		TaskAdapter adapter = new TaskAdapter(DisciplineActivity.this, tasks);
 		listView.setAdapter(adapter);
-		
+
 		listView.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
@@ -50,29 +51,31 @@ public class DisciplineActivity extends Activity {
 				startActivity(intent);
 			}
 		});
+
+		discipline.makeChartLayout(this, (LinearLayout) findViewById(R.id.item_chart_linear_layout));
 	}
-	
+
 	@Override
 	protected void onDestroy() {
-		if(tPersister != null){
+		if (tPersister != null) {
 			try {
 				tPersister.close();
 			} catch (Exception e) {
 				Log.e("DisciplineActivity", e.getMessage());
 			}
 		}
-		
+
 		super.onDestroy();
 	}
-	
+
 	@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	switch (item.getItemId()) {
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
 		case android.R.id.home:
 			NavUtils.navigateUpFromSameTask(this);
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
 		}
-    }
+	}
 }
